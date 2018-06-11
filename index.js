9545017595e3f2ddee3b6b4f6524c71adfa7a1c0
finalight <finalight@gmail.com>
2018-06-11 15:10:06 +0800

    change to sync download because website will block concurrent request

--- index.js
+++ index.js
@@ -4,29 +4,32 @@ const request = require('request')
 const cheerio = require('cheerio')
 const moment = require('moment')
 const downloader = require('image-downloader')
-const start_date = moment("10-01-2017", "DD-MM-YYYY")
-const end_date = moment("09-04-2017", "DD-MM-YYYY")
+const start_date = moment("01-12-2017", "DD-MM-YYYY")
+const end_date = moment("15-12-2017", "DD-MM-YYYY")
 let current_date = start_date
 let dates_array = []
 
 const downloadImage = (date) => {
-    request('http://dilbert.com/strip/' + current_date.format('YYYY-MM-DD'), (error, response, html) => {
-        if (!error && response.statusCode === 200) {
-            const $ = cheerio.load(html);
+    return new Promise((resolve, reject) => {
+        request('http://dilbert.com/strip/' + current_date.format('YYYY-MM-DD'), (error, response, html) => {
+            if (!error && response.statusCode === 200) {
+                const $ = cheerio.load(html);
 
-            // Download to a directory and save with the original filename
-            const options = {
-                url: $('.img-comic').attr('src'),
-                dest: './' + date + ".jpg", // Save to /path/to/dest/image.jpg
-                done: function(err, filename) {
-                    if (err) {
-                    	console.log(err)
-                    }
-                    console.log('File saved to', filename)
+                const options = {
+                    url: $('.img-comic').attr('src'),
+                    dest: './' + date + ".jpg", // Save to /path/to/dest/image.jpg
                 }
+
+                return downloader
+                    .image(options)
+                    .then(({filename, image}) => {
+                        return resolve('File saved to' + filename)
+                    })
+                    .catch((err) => {
+                        return reject(err)
+                    })
             }
-            downloader(options)
-        }
+        })
     })
 }
 
@@ -36,6 +39,17 @@ while (current_date <= end_date) {
     current_date = moment(current_date).add(1, 'days');
 }
 
-dates_array.forEach((date) => {
-    downloadImage(date)
-})
+const donwloadStrips = async() => {
+    console.log("size", dates_array.length)
+    for (let date of dates_array) {
+        console.log("downloading", date)
+        try {
+            const result = await downloadImage(date)
+            console.log(result)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+donwloadStrips()
